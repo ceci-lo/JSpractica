c2826dbfb917a752fba04023fb8f91ea1a44fddd
cecilia <lcecilia8147@gmail.com>
2023-01-26 10:42:05 -0300

    se resuleve ejercicio 1

---
+++ cursoUTN.js
@@ -0,0 +1,14 @@
+console.log("hello world!");
+//EJERCICIOS OPTATIVOS JS1
+
+//ejercicio 1
+let nombre = "";
+let apellidos = "";
+let edad = 0;
+
+nombre = prompt("ingrese su nombre");
+apellidos = prompt("ingrese su apellido");
+edad = prompt("ingrese su edad");
+console.log(
+  "se llama " + nombre + " se apellida " + apellidos + " su edad es de " + edad
+);
