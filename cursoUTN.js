6b3a0079657810f5abb9378f8869dca41cd815c6
cecilia <lcecilia8147@gmail.com>
2023-01-26 11:08:19 -0300

    se comenta cod

--- cursoUTN.js
+++ cursoUTN.js
@@ -6,9 +6,9 @@ let nombre = "";
 let apellidos = "";
 let edad = 0;
 
-nombre = prompt("ingrese su nombre");
-apellidos = prompt("ingrese su apellido");
-edad = prompt("ingrese su edad");
+//nombre = prompt("ingrese su nombre");
+//apellidos = prompt("ingrese su apellido");
+//edad = prompt("ingrese su edad");
 console.log(
   "se llama " + nombre + " se apellida " + apellidos + " su edad es de " + edad
 );
