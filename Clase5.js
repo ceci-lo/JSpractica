517db88bced620c4e3497bc1aba052062f8b7464
cecilia <lcecilia8147@gmail.com>
2023-02-08 07:51:27 -0300

    clase 5 condicionales

---
+++ Clase5.js
@@ -0,0 +1,14 @@
+//condicion
+//operador ternario
+//condicion ? accionTrue : accionFalse;
+
+//if-else (evalua una condicion, si se cumple sigue un camino o no)
+//si el usuario es mayor
+
+let edad = 16;
+let adulto = true;
+if (edad > 18 || adulto == true) {
+  console.log("tiene acceso al sitio");
+} else {
+  console.log("necesitas un adulto responsanble para ingresar");
+}
