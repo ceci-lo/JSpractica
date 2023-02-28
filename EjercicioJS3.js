33d54747a95d213df67e208ad9d507985ba69fad
cecilia <lcecilia8147@gmail.com>
2023-02-28 16:40:20 -0300

    Se resulven ejercios guia 3 js

--- EjercicioJS3.js
+++ EjercicioJS3.js
@@ -18,7 +18,7 @@
 //EJERCICIO 1
 let elementA = document.getElementsByTagName("a").length;
 
-console.log(elementA);
+
 
 //EJERCICIO 2
 let body = document.getElementsByTagName("body");
@@ -29,3 +29,36 @@ imagen.setAttribute(
   "/img/cherry-blossoms-in-spring-4a3alz4ak7x9q6gy.jpg"
 );
 body[0].insertBefore(imagen, texto);
+
+//EJERCICIO 3
+texto.innerHTML = (' Documentos es una interfaz entre JavaScript y HTML o XML (otro metalenguaje que no abordamos en este curso). Nos permite acceder a sus partes y propiedades e ir interactuando con ellas.</br>Tenemos que tener en cuenta que cuando hablamos de propiedades de las partes del HTML estamos  hablando de su CSS por lo que el DOM nos va a permitir modificar el CSS también, por medio de HTML')
+
+//EJERCICIO4
+// primera forma 
+/*let ul = document.getElementById("listado"); 
+ul.className = "menu"*/
+
+//segunda forma
+let menu = document.getElementsByTagName("ul");
+menu[0].classList.add("menu")
+
+// EJERCICIO 5
+let tituloNuevo = document.getElementsByTagName("h1");
+tituloNuevo[0].innerHTML = 'Introducción del DOM' // también funciona con textContent
+
+// EJERCICIO 6
+let metodos =["createElement()","appendChild()", "insertBefore()", "removeChild()","replaceChild()", "removeAttribute()","classList.add()","classList.remove()","classList.toggle()"]
+
+let h2 = document.getElementsByTagName("h2");
+ let ul = document.createElement("ul");
+for(let element of metodos){
+ let listado = document.createElement("li");
+  let txt = document.createTextNode(element);
+
+listado.appendChild(txt);
+ul.appendChild(listado);
+
+h2[0].insertBefore(ul, null);
+ 
+}
+
